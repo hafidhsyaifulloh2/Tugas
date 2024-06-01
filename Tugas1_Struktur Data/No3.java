04eacb71820782b8d835f351b6796af37b6436ee
Hafidh Syaifullah <162431999+hafidhsyaifulloh2@users.noreply.github.com>
2024-06-01 08:57:39 +0700

    Add files via upload

---
+++ Tugas1_Struktur Data/No3.java
@@ -0,0 +1,7 @@
+public class No3 {
+    public static void main(String[] args) {
+        int [] empatAngka = {07,10,20,28};
+        System.out.println("empatAngka : ");
+        for(int num : empatAngka) System.out.println(num + " ");
+    }   
+}
